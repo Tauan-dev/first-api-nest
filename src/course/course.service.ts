cfbf73ef09aaa4af036baee93fa9867950d4dbc0
Tauan Neres <tauanspider@gmail.com>
2024-02-01 09:40:48 -0300

    "CRUD-service"

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException } from "@nestjs/common";
 import { Course } from "./entities/course.entity";
 import { Repository } from "typeorm";
 import { InjectRepository } from "@nestjs/typeorm";
+import { UpdateCourse } from "./dto/update-course.dto";
 
 @Injectable()
 export class CourseService {
@@ -15,32 +16,41 @@ export class CourseService {
   //METHODS CRUDING
 
   async findAll() {
-    return this.courseRepository.find();
+    return await this.courseRepository.find();
   } // todos os métodos estão realizando operações assíncronas (se observar o retorno é uma promisse), ou seja isso vai levar um tempo até acontecer, ao acessar o banco de dados e depois retornar a resposta, por esse motivo os métodos serão assincronos
 
-  findOne(id: number) {
-    return this.courseRepository.findOne(id);
-  }
-  // percorre cada item de dentro de um array e retorna os dados com id correspondente
-
-  create(createCourse: any) {
-    this.courseRepository.create();
-  }
-
-  update(id: number, updateCourse: any) {
-    const existingCourse = this.findOne(id);
-    if (existingCourse) {
-      const index = this.courses.findIndex((course) => course.id === id);
-      this.courses[index] = {
-        ...updateCourse,
-      };
+  async findOne(id: number) {
+    const course = await this.courseRepository.findOne({ where: { id } });
+    if (!course) {
+      throw new NotFoundException(`Course whit ID ${id} not found`);
     }
+    // o !course vai sempre garantir que o course a ser manipulado existe
+    return course;
   }
 
-  remove(id: number) {
-    const index = this.courses.findIndex((course) => course.id === id);
-    if (index >= 0) {
-      this.courses.splice(index, 1);
+  async create(createCourse: any) {
+    const course = this.courseRepository.create(createCourse); // o método não é assincrono pois só cria a instância/objeto com base nos parametros (entidade)
+    return this.courseRepository.save(course); // salva a instância criada acima
+  }
+
+  async update(id: number, updateCourse: any) {
+    const course = await this.courseRepository.preload({
+      ...updateCourse,
+      id,
+    });
+    if (!course) {
+      throw new NotFoundException(`Course whit id ${id} not found	`);
     }
+    return this.courseRepository.save(course); // salva a instância criada
+  }
+
+  async remove(id: number) {
+    const course = await this.courseRepository.findOne({
+      where: { id },
+    });
+    if (!course) {
+      throw new NotFoundException(`Course not found`);
+    }
+    return this.courseRepository.remove(course); // deleta o dado
   }
 }
