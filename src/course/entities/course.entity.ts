fe5c626fcb7850e39a3add859e99811abbb757f2
Tauan Neres <tauanspider@gmail.com>
2024-02-01 11:57:59 -0300

    "add-relationship-bettwen-tables"

--- src/course/entities/course.entity.ts
+++ src/course/entities/course.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Tag } from "./tag.entity";
 
 @Entity("courses") //esse parametro passado no decorator @Entity, vai ser o nome da tabela dentro do banco de dados
 export class Course {
@@ -11,8 +12,10 @@ export class Course {
   @Column()
   description: string;
 
-  @Column("json", { nullable: true })
-  tags: Array<string>;
+  @JoinTable() // usado na tabela principal em uma relação ManyToMany para indicar quem é o proprietário da relação
+  @ManyToMany(() => Tag, (tag) => tag.courses)
+  tags: Tag[];
+  // os parametros recebidos são respectivamente () => Tag, sendo a entidade relacionada e (tag) => tag.courses que é o inverseSide, ou seja a propriedade course na entidade tag
 }
 
 //para a entitie se tornar uma entidade do typeorm é utilizado o decorator @Entity
